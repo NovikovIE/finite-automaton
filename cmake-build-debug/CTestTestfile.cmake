ce8ba1e161bf444aa84dbc1394a8e2476741ee7c
NovikovIE <ivannovikov1303@gmail.com>
2023-01-27 15:14:43 +0300

    edit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Program Files (x86)/github/FiniteAutomaton
+# Build directory: C:/Program Files (x86)/github/FiniteAutomaton/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
