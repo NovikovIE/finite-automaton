d892138e7d06cbb64d053804eb644e4e86a471f3
NovikovIE <91309400+NovikovIE@users.noreply.github.com>
2023-01-27 15:17:16 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Program Files (x86)/github/FiniteAutomaton
-# Build directory: C:/Program Files (x86)/github/FiniteAutomaton/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
